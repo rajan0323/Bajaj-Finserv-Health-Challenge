349ef05a976a123ddbedfda2561b97e7d80ef46c
Rajan kumar <133194869+rajan0323@users.noreply.github.com>
2024-08-25 12:39:14 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const connectDB = require("./db/dbConnect");
+const bfhlRoutes = require("./routes/bfhl");
+require("dotenv").config();
+
+const cors = require("cors")
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+app.use(cors())
+app.use(bodyParser.json());
+
+// Routes
+app.use("/api", bfhlRoutes);
+
+// Server Setup
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
